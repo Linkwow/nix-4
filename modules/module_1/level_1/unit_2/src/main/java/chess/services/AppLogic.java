5980c5df8af711323365e56d8f778790db60129b
Serhii Naumenko <Linkwow75@gmail.com>
2021-03-23 21:04:10 +0200

    change application logic to create infinitive chess board

--- modules/module_1/level_1/unit_2/src/main/java/chess/services/AppLogic.java
+++ modules/module_1/level_1/unit_2/src/main/java/chess/services/AppLogic.java
@@ -5,31 +5,19 @@ public class AppLogic {
     public static Object[] startPosition(String input) {
         Object[] array = new Object[2];
         char letterPosition = input.charAt(0);
-        int digitPosition = Integer.parseInt(String.valueOf(input.charAt(1)));
+        int digitPosition = Integer.parseInt(input,1,input.length(), 10);
         if (Character.isLowerCase(letterPosition)) {
             letterPosition = Character.toUpperCase(letterPosition);
         }
-        if (startPositionCheck(letterPosition, digitPosition)) {
-            array[0] = letterPosition;
-            array[1] = digitPosition;
-        } else {
-            throw new ArrayIndexOutOfBoundsException("Вы ввели недопустимые значения.");
-        }
+        array[0] = letterPosition;
+        array[1] = digitPosition;
         return array;
-    }
-
-    private static boolean startPositionCheck(char ch, int i) {
-        if ((ch >= 65 && ch <= 72) && (i >= 1 && i <= 8)) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public static Object[] nextPosition(String input, Object[] currentPosition) {
         Object[] array = new Object[2];
         char letterPosition = input.charAt(0);
-        int digitPosition = Integer.parseInt(String.valueOf(input.charAt(1)));
+        int digitPosition = Integer.parseInt(input,1,input.length(), 10);
         if (Character.isLowerCase(letterPosition)) {
             letterPosition = Character.toUpperCase(letterPosition);
         }
@@ -45,11 +33,44 @@ public class AppLogic {
     private static boolean nextPositionCheck(char ch, int i, Object[] currentPosition) {
         char letterPosition = (char) currentPosition[0];
         int digitPosition = (int) currentPosition[1];
-        if ((ch >= 65 && ch <= 72) && (i >= 1 && i <= 8)) {
-            if (((letterPosition - 1 == ch) && (letterPosition - 1 >= 65)) && (digitPosition - 2 == i) && (digitPosition - 2 >= 1) ||
-                    ((letterPosition + 1 == ch) && (letterPosition - 1 <= 72)) && (digitPosition + 2 ==i) && (digitPosition + 2 <= 8)){
+        if ((ch == letterPosition + 1) || (ch == letterPosition - 1)) {
+            if (i == digitPosition + 2 || i == digitPosition - 2) {
                 return true;
             } else {
+                return false;
+            }
+        } else if (ch == letterPosition + 2 || ch == letterPosition - 2) {
+            if (i == digitPosition + 1 || i == digitPosition - 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (ch < letterPosition + 1 && ch == 'A') {
+            if (i == digitPosition + 2 || i == digitPosition - 2) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (ch < letterPosition + 1 && ch == 'B') {
+            if (i == digitPosition + 1 || i == digitPosition - 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (ch > letterPosition + 1 && ch == 'Z') {
+            if (i == digitPosition + 2 || i == digitPosition - 2) {
+                return true;
+            } else if (i == digitPosition + 1 || i == digitPosition - 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (ch > letterPosition + 1 && ch == 'Y') {
+            if (i == digitPosition + 1 || i == digitPosition - 1) {
+                return true;
+            } else if (i == digitPosition + 2 || i == digitPosition - 2){
+                return true;
+            } else{
                 return false;
             }
         } else {
@@ -57,8 +78,8 @@ public class AppLogic {
         }
     }
 
-    public static boolean endCalculate(String s){
-        if(Integer.parseInt(s) == 1 ){
+    public static boolean endCalculate(String s) {
+        if (Integer.parseInt(s) == 1) {
             return false;
         } else {
             return true;
