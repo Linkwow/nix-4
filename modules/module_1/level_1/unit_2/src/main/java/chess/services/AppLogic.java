3f57dda89a5239a84560c3bf7b25acb1e9211a99
Serhii Naumenko <Linkwow75@gmail.com>
2021-03-26 19:40:23 +0200

    the cycle for infinitive numbers was added

--- modules/module_1/level_1/unit_2/src/main/java/chess/services/AppLogic.java
+++ modules/module_1/level_1/unit_2/src/main/java/chess/services/AppLogic.java
@@ -33,6 +33,11 @@ public class AppLogic {
     private static boolean nextPositionCheck(char ch, int i, Object[] currentPosition) {
         char letterPosition = (char) currentPosition[0];
         int digitPosition = (int) currentPosition[1];
+        if(digitPosition + i < 0){
+            digitPosition = 0;
+        } else if( digitPosition - i < 0){
+            digitPosition = Integer.MAX_VALUE - 2;
+        }
         if ((ch == letterPosition + 1) || (ch == letterPosition - 1)) {
             if (i == digitPosition + 2 || i == digitPosition - 2) {
                 return true;
