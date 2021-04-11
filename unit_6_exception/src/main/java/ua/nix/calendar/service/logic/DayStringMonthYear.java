3ed7ec1897345233c673bddf7e8bf990f71670c6
Serhii Naumenko <Linkwow75@gmail.com>
2021-04-11 11:01:07 +0300

    Not full but most :)

--- unit_6_exception/src/main/java/ua/nix/calendar/service/logic/DayStringMonthYear.java
+++ unit_6_exception/src/main/java/ua/nix/calendar/service/logic/DayStringMonthYear.java
@@ -21,16 +21,16 @@ public class DayStringMonthYear {
             Map.entry("Декабрь", 13));
 
     public static String addTime(String input) {
-        String[] array = new String[]{"", "", "", "", ""};
+        String[] array = new String[]{"", "", "", "", "", "", ""};
         String[] temp = input.split("[ :]");
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < temp.length; i++) {
-            array[i] = temp[i];
+        for (int z = 0; z < temp.length; z++) {
+            array[z] = temp[z];
         }
         for (int i = 0; i < array.length; i++) {
-            if (i == 0) {
+            if (i == 0 || i == 1 || i == 2) {
                 array[i] += " ";
-            } else if (i == 1 || i == 2 || i == 3) {
+            } else if (i < array.length - 1) {
                 if (array[i].isEmpty()) {
                     array[i] += "00:";
                 } else {
@@ -51,26 +51,6 @@ public class DayStringMonthYear {
     public static String[] createStringData(String result) throws DateException {
         String[] array = new String[7];
         String[] temp = result.split("[/ :]");
-        if (temp[1].isEmpty()) {
-            temp[1] = "01";
-        }
-        if (temp[2].isEmpty()) {
-            temp[2] = "2021";
-        } for (int i = 0; i < array.length; i++) {
-            if (i == 0) {
-                array[i] += " ";
-            } else if (i == 1 || i == 2 || i == 3) {
-                if (array[i].isEmpty()) {
-                    array[i] += "00:";
-                } else{
-                    array[i] += ":";
-                }
-            } else {
-                if (array[i].isEmpty()) {
-                    array[i] += "00";
-                }
-            }
-        }
         array[0] = temp[1];
         array[1] = String.valueOf(monthMap.get(temp[0]));;
         if (array[1] == "null") {
