0332cfaf81b53a538d5744d91b834d7dcc04c4ee
Serhii Naumenko <Linkwow75@gmail.com>
2021-04-18 02:30:12 +0300

    v.00.00 complete task 7 with few exceptions and demo file and minor encoding changes

---
+++ unit_7_collection_1/app/src/main/java/ua/nix/collections/App.java
@@ -0,0 +1,9 @@
+package ua.nix.collections;
+
+import ua.nix.collections.controller.Controller;
+
+public class App {
+    public static void main(String[] args) {
+        Controller.run();
+    }
+}
--- unit_7_collection_1/app/src/main/java/ua/nix/collections/entity/baseentity/AbstractUser.java
+++ unit_7_collection_1/app/src/main/java/ua/nix/collections/entity/baseentity/AbstractUser.java
@@ -1,19 +1,10 @@
 package ua.nix.collections.entity.baseentity;
 
 public abstract class AbstractUser {
-    protected Integer id;
     protected String name;
 
     public AbstractUser(String name){
         this.name = name;
-    }
-
-    protected int stringValueCount(){
-        int count = 0;
-        for (Character c : this.name.toCharArray()){
-            count += c;
-        }
-        return count;
     }
 
     abstract public String toString();
