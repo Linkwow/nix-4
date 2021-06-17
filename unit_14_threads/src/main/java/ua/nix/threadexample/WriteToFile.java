ec925aad60c17d29a3a62ebd374e6b9b6aaeca8b
Serhii Naumenko <linkwow75@gmail.com>
2021-06-17 20:17:19 +0300

    minor changes

--- unit_14_threads/src/main/java/ua/nix/threadexample/WriteToFile.java
+++ unit_14_threads/src/main/java/ua/nix/threadexample/WriteToFile.java
@@ -9,11 +9,9 @@ import org.slf4j.LoggerFactory;
 
 public class WriteToFile implements Runnable {
 
-    private final String threadName = "FileWriter";
     private final Application application;
     private final File file = new File("src\\main\\java\\ua\\nix\\threadexample\\output.txt");
     private static Logger logger;
-    private String input;
 
     public WriteToFile(Application application) throws IOException {
         this.application = application;
@@ -24,16 +22,13 @@ public class WriteToFile implements Runnable {
     public void run() {
         synchronized (application) {
             try (FileWriter fileWriter = new FileWriter(file)) {
-                while (true) {
-                    if (application.isStop())
-                        break;
-                    input = application.getInput();
+                while (!application.isStop()) {
+                    String input = application.getInput();
                     application.wait(1000);
                     logger.debug("input hashCode" + input.hashCode());
                     logger.debug("application hashcode" + application.getInput().hashCode());
                     if (input.hashCode() != application.getInput().hashCode()) {
                         fileWriter.write(application.getInput());
-                        input = application.getInput();
                     }
                 }
             } catch (InterruptedException | IOException e) {
