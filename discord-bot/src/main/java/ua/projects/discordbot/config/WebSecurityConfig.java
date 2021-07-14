9f8388625ea7738794031f880ad533c5a6981aae
Serhii Naumenko <linkwow75@gmail.com>
2021-07-14 21:53:05 +0300

    preliminary final project design

--- discord-bot/src/main/java/ua/projects/discordbot/config/WebSecurityConfig.java
+++ discord-bot/src/main/java/ua/projects/discordbot/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ua.projects.discordbot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,11 +18,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     //fixme - use
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("**/admin/**")
-                .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated().and().httpBasic().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers("totalWarWarhammer/user/showRaceId");
+                .antMatchers(HttpMethod.GET).permitAll()
+                .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                .anyRequest().authenticated().and().httpBasic().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
     @Override
